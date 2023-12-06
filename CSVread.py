a42f1d9b4029b646e6986ab667f11ba6430a2f9b
ameer <ameernavas69@gmail.com>
2023-12-06 07:21:03 +0530

    small mistake

--- CSVread.py
+++ CSVread.py
@@ -1,5 +1,10 @@
 import csv
 import sqlite3
+import random
+import os
+
+if os.path.exists('DATA/booksDB.db'):
+    os.remove('DATA/booksDB.db')
 
 conn = sqlite3.connect('DATA/booksDB.db')
 cur = conn.cursor()
@@ -7,13 +12,19 @@ cur = conn.cursor()
 
 
 cur.execute('''CREATE TABLE IF NOT EXISTS booksTB (
-                  ID INTEGER PRIMARY KEY,
-                  Title TEXT,
-                  Author TEXT,
-                  Genre TEXT
+                id INTEGER PRIMARY KEY,
+                title TEXT,
+                author TEXT,
+                genre TEXT,
+                publisher
+                Stocks INTEGER
                 )'''
             )
-# d = cur.execute(''' CREATE TABLE IF NOT EXISTS ''')
+cur.execute(''' CREATE TABLE IF NOT EXISTS Customer (
+            book_id INTEGER,
+            customer_name TEXT,
+            FOREGN KEY(book_id) REFERENCES booksTB
+)''')
 
 
 with open('DATA/books1.csv', 'r') as file:
@@ -22,8 +33,12 @@ with open('DATA/books1.csv', 'r') as file:
 
 
     for row in csv_reader:
-        d = cur.execute('INSERT INTO booksTB (Title, Author, Genre) VALUES (? , ?, ?)', (row[0], row[1], row[2]))
-        print([a for a in d])
+
+        cur.execute('INSERT INTO booksTB (title, author, genre, stocks) VALUES (? , ?, ?, ?)', (row[0], row[1], row[2]), random.randint(0,10))
+        bkid = cur.lastrowid()
+        print(bkid)
+
+        
 
 conn.commit()
 conn.close()
