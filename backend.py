83aa8883befb42fd4e54d3820e89af7777a25b59
ameer <ameernavas69@gmail.com>
2023-12-06 07:51:14 +0530

    borrow_book function added

--- backend.py
+++ backend.py
@@ -13,6 +13,18 @@ def check_availabiliy(book):
     cur.execute('SELECT id, title, genre FROM booksTB where Title LIKE ?', ('%' + book + '%',))
     return [results for results in cur.fetchall()]
 
+def borrow_book(book):
+    cur.execute("SELECT stock FROM booksTB WHERE title = (?)", book)
+    out = cur.fetchone
+    id, stock = (out[0], out[4])
+
+    if not stock:
+        return "Out Of Stock"
+    #in stock
+    else:
+        #decrease stock
+        cur.execute("UPDATE booksTB SET stock = stock - 1 WHERE id = (?)", id)
+        return id
 
 
 
